e9aa67fc7acf5904435f62397ba05e03b09cb536
mario-aleo <marioaleo@gmail.com>
2016-03-28 15:14:56 -0300

    Update .gitignore

--- platforms/android/assets/www/app/shared/shared-service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-angular.module("ngapp").service("shared", function(){ // One of The Ways To Share Informations Across the Controllers
-
-    this.info = {
-        title: "cordova-angular-angularMaterial-seed",
-        auth: "Mario Aleo"
-    };
-});
